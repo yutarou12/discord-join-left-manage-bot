064c2b1bc5e77d078b9e3eca916c629ed2e709cc
yutarou12 <sasaki.yuutarou12@gmail.com>
2026-06-11 11:10:09 +0900

    [add] データベース化

--- cogs/joinleft.py
+++ cogs/joinleft.py
@@ -1,41 +1,33 @@
 from discord.ext import commands
 
-from libs.origin_handler import GuildDataModel
+from libs.origin_handler import JoinLeftNoticeModel
 
 
 class JoinLeft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.guild_list = [881390536504799234, 1339605648975069287]
-        self.guild_data: dict[int, GuildDataModel] = {}
-
-    def setup(self, bot):
-        self.guild_data.update({1339605648975069287:GuildDataModel(notice_bool=True, join_notice_channel_id=1513502054528712804, left_notice_channel_id=1513502054528712804)})
+        self.db = bot.db
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         _guild = member.guild
 
-        self.setup(self.bot)
+        data: JoinLeftNoticeModel = await self.db.get_join_notice(_guild.id)
 
-        if _guild.id not in self.guild_list:
-            # Handle member join logic for specific guilds
+        if not data.function:
             return None
 
-        if not self.guild_data.get(_guild.id):
-            return None
+        if bool(data.channel_id):
+            channel = member.guild.get_channel(data.channel_id)
+            if channel is None:
+                channel = await member.guild.fetch_channel(data.channel_id)
 
-        if not self.guild_data.get(_guild.id).notice_bool:
-            return None
-
-        guild_data = self.guild_data.get(_guild.id)
-
-        channel = member.guild.get_channel(guild_data.join_notice_channel_id)
-        if channel is None:
-            channel = await member.guild.fetch_channel(guild_data.join_notice_channel_id)
-
-        if channel is None:
-            return None
+                if channel is None:
+                    return None
+        else:
+            if not _guild.system_channel:
+                return None
+            channel = _guild.system_channel
 
         return await channel.send(f"> 📥{member.mention} さんが入室しました")
 
@@ -43,26 +35,22 @@ class JoinLeft(commands.Cog):
     async def on_member_remove(self, member):
         _guild = member.guild
 
-        self.setup(self.bot)
+        data: JoinLeftNoticeModel = await self.db.get_left_notice(_guild.id)
 
-        if _guild.id not in self.guild_list:
-            # Handle member remove logic for specific guilds
+        if not data.function:
             return None
 
-        if not self.guild_data.get(_guild.id):
-            return None
+        if bool(data.channel_id):
+            channel = member.guild.get_channel(data.channel_id)
+            if channel is None:
+                channel = await member.guild.fetch_channel(data.channel_id)
 
-        if not self.guild_data.get(_guild.id).notice_bool:
-            return None
-
-        guild_data = self.guild_data.get(_guild.id)
-
-        channel = member.guild.get_channel(guild_data.left_notice_channel_id)
-        if channel is None:
-            channel = await member.guild.fetch_channel(guild_data.left_notice_channel_id)
-
-        if channel is None:
-            return None
+                if channel is None:
+                    return None
+        else:
+            if not _guild.system_channel:
+                return None
+            channel = _guild.system_channel
 
         return await channel.send(f"> 📤{member.mention} さんが退出しました")
 
